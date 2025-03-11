248c07cd17ce9652fd82a3966b621683247f6653
JOUUUSKA <1393827820@qq.com>
2025-03-11 17:04:40 +0800

    update/update_downloader(spider_toolsbox): 在BaseClient基类中新增了iter_content属性, 在BaseRequest类中新增了iter_content属性和headers属性,
    更新了下载器的冗余代码，
    优化了
    新增了

--- tools/download/dowloader.py
+++ tools/download/dowloader.py
@@ -1,8 +1,39 @@
-from spider_toolsbox.tools.link_extractors.html_response import extract_text_by_response_xpath
+import os
+from urllib.parse import urljoin
+
+from spider_toolsbox.tools.link_extractors.html_response import (
+    extract_text_by_response_xpath,
+    extract_link_by_response_xpath
+)
 from spider_toolsbox.tools.log import success, critical
 from spider_toolsbox.tools.other_tools.name import video_name, image_name, txt_name
-from spider_toolsbox.tools.request import create_default_headers, Request
-from spider_toolsbox.tools.utils.url import is_attachment_href
+from spider_toolsbox.tools.other_tools.name import file_name as download_file_name
+from spider_toolsbox.tools.request import create_default_headers, create_request
+from spider_toolsbox.tools.utils.url import is_attachment_href, is_valid_url
+
+
+class DownloadError(Exception):
+    """自定义下载异常"""
+    pass
+
+
+def save_response_content(
+        response,
+        file_name=None,
+        file_type=None,
+        file_path=None,
+        mode=None,
+):
+    if response.status_code == 200:
+        if file_path is None:
+            with open(fr"./{file_name}.{file_type}", mode) as f:
+                f.write(response.content)  # type:ignore
+        else:
+            with open(fr"{file_path}/{file_name}.{file_type}", mode) as f:
+                f.write(response.content)  # type:ignore
+        success(f"{file_name}.{file_type} 下载成功")
+    else:
+        critical(f"{file_name}.{file_type} 下载失败")
 
 
 def download_video(
@@ -11,11 +42,10 @@ def download_video(
         headers=None,
         data=None,
         params=None,
-        name=None,
-        file_type=None,
-        mode=None,
-        file_path=None,
         file_name=None,
+        file_type=None,
+        file_path=None,
+        mode=None,
         **kwargs
 ):
     """
@@ -23,7 +53,7 @@ def download_video(
     :param headers: 请求头，如果没有指定，则默认使用FakeUA库进行创建使用
     :param data: 提交表单，如果没有指定，则默认为None
     :param params: 参数，如果没有指定，则默认为None
-    :param name: 下载时文件的命名，如果没有指定，则默认为字符串形式的时间戳
+    :param file_name: 下载时文件的命名，如果没有指定，则默认为字符串形式的时间戳
     :param file_type: 下载文件的后缀名，如果没有指定，则默认为mp4
     :param mode: 下载文件时的模式，如果没有指定，则默认为wb
     :return:None
@@ -38,22 +68,20 @@ def download_video(
     那么:
     headers 默认为 headers
     file_type 默认为 'mp4'
-    name 默认为 str(video_name())
+    file_name 默认为 str(video_name())
     mode 默认为 'wb'
 
     并在此文件夹下新建一个Download_video文件夹，对下载的视频进行持久化存储
     """
     if file_type is None:
         file_type = "mp4"
-    if name is None:
-        name = str(video_name())
     if mode is None:
         mode = "wb"
     if headers is None:
         headers = create_default_headers()
     if file_name is None:
-        file_name = video_name()
-    response = Request(
+        file_name = str(video_name())
+    response = create_request(
         url,
         method=method,
         headers=headers,
@@ -61,27 +89,88 @@ def download_video(
         params=params,
         **kwargs
     )
-    if response.status_code == 200:
-        if file_path is None:
-            with open(f"./{file_name}.{file_type}", mode) as f:
-                f.write(response.content)  # type:ignore
-        else:
-            with open(f"{file_path}/{file_name}.{file_type}", mode) as f:
-                f.write(response.content)  # type:ignore
-        success(f"{file_name}.{file_type} 下载成功")
-    else:
-        critical(f"{name}.{file_type} 下载失败")
+    save_response_content(
+        response,
+        file_name,
+        file_type,
+        file_path,
+        mode,
+    )
+
+
+def download_m3u8(
+        m3u8_url: str,
+        output_name: str = "output",
+        file_path: str = None,
+        max_workers: int = 8,
+        **kwargs
+) -> str:
+    """
+    M3U8流媒体下载器
+    :param m3u8_url: m3u8索引文件地址
+    :param output_name: 输出文件名（不含后缀）
+    :param file_path: 存储路径
+    :param max_workers: 最大并发线程数
+    :return: 合并后的文件路径
+    """
+    from concurrent.futures import ThreadPoolExecutor
+
+    # 获取m3u8内容
+    response = create_request(m3u8_url, **kwargs)
+    if response.status_code != 200:
+        raise DownloadError(f"无法获取M3U8文件，状态码：{response.status_code}")
+
+    # 解析TS列表
+    ts_list = [
+        urljoin(m3u8_url, line.strip())
+        for line in response.text.split("\n")
+        if line.strip() and not line.startswith("#")
+    ]
+
+    # 下载所有TS片段
+    file_path = file_path or os.getcwd()
+    ts_dir = os.path.join(file_path, "ts_temp")
+    os.makedirs(ts_dir, exist_ok=True)
+
+    def _download_ts(ts_url: str, index: int):
+        ts_path = os.path.join(ts_dir, f"seg_{index:04d}.ts")
+        with create_request(ts_url, stream=True, **kwargs) as r:
+            with open(ts_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=1024 * 1024):
+                    if chunk:
+                        f.write(chunk)
+        return ts_path
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [
+            executor.submit(_download_ts, url, idx)
+            for idx, url in enumerate(ts_list)
+        ]
+        for future in futures:
+            future.result()
+
+    # 合并TS文件
+    output_path = os.path.join(file_path, f"{output_name}.mp4")
+    with open(output_path, "wb") as out_f:
+        for idx in range(len(ts_list)):
+            seg_path = os.path.join(ts_dir, f"seg_{idx:04d}.ts")
+            with open(seg_path, "rb") as in_f:
+                out_f.write(in_f.read())
+            os.remove(seg_path)
+
+    os.rmdir(ts_dir)
+    success(f"M3U8文件合并完成：{output_path}")
+    return output_path
 
 
 def download_file(
         file_url,
-        file_name,
         file_type,
+        file_name=None,
         method="get",
         headers=None,
         data=None,
         params=None,
-        name=None,
         mode=None,
         file_path=None,
         **kwargs
@@ -111,20 +200,20 @@ def download_file(
     name 默认为 str(video_name())
     mode 默认为 'wb'
 
-    并在此文件夹下新建一个Download_file文件夹，对下载的视频进行持久化存储
+    并在此文件夹下新建一个Download_file文件夹，对下载的文件进行持久化存储
     """
-    if is_attachment_href(file_url):
+    if is_attachment_href(file_url) or is_valid_url(file_url):
         checked_file_url = file_url
     else:
         raise ValueError("Invalid file url")
 
-    if name is None:
-        name = str(video_name())
+    if file_name is None:
+        file_name = str(download_file_name())
     if mode is None:
         mode = "wb"
     if headers is None:
         headers = create_default_headers()
-    response = Request(
+    response = create_request(
         checked_file_url,
         method=method,
         headers=headers,
@@ -132,16 +221,13 @@ def download_file(
         params=params,
         **kwargs
     )
-    if response.status_code == 200:
-        if file_path is None:
-            with open(f"./{file_name}.{file_type}", mode) as f:
-                f.write(response.content)  # type:ignore
-        else:
-            with open(f"{file_path}/{file_name}.{file_type}", mode) as f:
-                f.write(response.content)  # type:ignore
-        success(f"{file_name}.{file_type} 下载成功")
-    else:
-        critical(f"{name}.{file_type} 下载失败")
+    save_response_content(
+        response,
+        file_name,
+        file_type,
+        file_path,
+        mode,
+    )
 
 
 def download_img(
@@ -150,11 +236,10 @@ def download_img(
         headers=None,
         data=None,
         params=None,
-        name=None,
+        file_name=None,
         file_type=None,
         mode=None,
         file_path=None,
-        file_name=None,
         **kwargs
 ):
     """
@@ -162,7 +247,7 @@ def download_img(
     :param headers: 请求头，如果没有指定，则默认使用FakeUA库进行创建使用
     :param data: 提交表单，如果没有指定，则默认为None
     :param params: 参数，如果没有指定，则默认为None
-    :param name: 下载时文件的命名，如果没有指定，则默认为字符串形式的时间戳
+    :param file_name: 下载时文件的命名，如果没有指定，则默认为字符串形式的时间戳
     :param file_type: 下载文件的后缀名，如果没有指定，则默认为jpg
     :param mode: 下载文件时的模式，如果没有指定，则默认为wb
     :return:None
@@ -184,13 +269,13 @@ def download_img(
     """
     if file_type is None:
         file_type = "jpg"
-    if name is None:
-        name = str(image_name())
+    if file_name is None:
+        file_name = str(image_name())
     if mode is None:
         mode = "wb"
     if headers is None:
         headers = create_default_headers()
-    response = Request(
+    response = create_request(
         url,
         method=method,
         headers=headers,
@@ -198,104 +283,101 @@ def download_img(
         params=params,
         **kwargs
     )
-    if response.status_code == 200:
-        if file_path is None:
-            with open(f"./{file_name}.{file_type}", mode) as f:
-                f.write(response.content)  # type:ignore
-        else:
-            with open(f"{file_path}/{file_name}.{file_type}", mode) as f:
-                f.write(response.content)  # type:ignore
-        success(f"{file_name}.{file_type} 下载成功")
-    else:
-        critical(f"{name}.{file_type} 下载失败")
+    save_response_content(
+        response,
+        file_name,
+        file_type,
+        file_path,
+        mode,
+    )
 
 
-def download_character_by_response_xpath(
-        url,
-        xpath,
-        method="get",
-        headers=None,
-        data=None,
-        params=None,
+def download_text(
+        text,
         file_name=None,
         file_type=None,
-        file_path=None,
         mode=None,
+        file_path=None,
+        encoding="utf-8",
         **kwargs
 ):
-    """
-    :param txt: 需要下载的 文字 或 响应
+    '''
+    为下载其他数据提供一个简洁的api
+
+    :param text: 需要下载的 数据
     :param name: 下载时文件的命名，如果没有指定，则默认为字符串形式的时间戳
     :param file_type: 下载文件的后缀名，如果没有指定，则默认为txt
     :param mode: 下载文件时的模式，如果没有指定，则默认为w
     :return: None
-
-
-    此函数用于在爬虫任务中，
-    通过一个给定的文本，快速地进行 文本 下载操作
-
-    TXT为必填，其他参数为选填，
-    如果:
-    对其他参数没有进行特别指定，
-    那么:
-    file_type 默认为 'txt'
-    name 默认为 str(txt_name())
-    mode 默认为 'w'
-
-    并在此文件夹下新建一个Download_txt文件夹，对下载的 文本 进行持久化存储
-    """
+    '''
     if file_type is None:
         file_type = "txt"
     if file_name is None:
         file_name = str(txt_name())
     if mode is None:
         mode = "w"
-    req = Request(
-        url,
-        method=method,
-        headers=headers,
-        data=data,
-        params=params,
-        **kwargs
-    )
-    text = extract_text_by_response_xpath(req, xpath)  # type: ignore
-
-    if file_path is None:
-        with open(f"./{file_name}.{file_type}", mode) as f:
+    if file_path:
+        with open(fr"{file_path}/{file_name}.{file_type}", mode, encoding=encoding) as f:
             f.write(text)  # type:ignore
     else:
-        with open(f"{file_path}/{file_name}.{file_type}", mode) as f:
+        with open(fr"./{file_name}.{file_type}", mode, encoding=encoding) as f:
             f.write(text)  # type:ignore
     success(f"{file_name}.{file_type} 下载成功")
 
 
-def download_others(
-        others,
-        name=None,
-        file_type=None,
-        mode=None,
-        file_path=None,
+def download_text_by_response_xpath(
+        response,
+        xpath,
+        split="\n",
         **kwargs
 ):
-    '''
-    为下载其他数据提供一个简洁的api
-
-    :param others: 需要下载的 数据
-    :param name: 下载时文件的命名，如果没有指定，则默认为字符串形式的时间戳
-    :param file_type: 下载文件的后缀名，如果没有指定，则默认为txt
-    :param mode: 下载文件时的模式，如果没有指定，则默认为w
+    """
+    :param response: 网页response
+    :param xpath: 网页中需要下载的文本元素xpath
+    :param split: 分隔符类型, 默认为换行符
     :return: None
-    '''
-    if file_type is None:
-        file_type = "txt"
-    if name is None:
-        name = str(txt_name())
-    if mode is None:
-        mode = "w"
-    if file_path is None:
-        with open(f"./{name}.{file_type}", mode) as f:
-            f.write(text)  # type:ignore
-    else:
-        with open(f"{file_path}/{name}.{file_type}", mode) as f:
-            f.write(others)  # type:ignore
-    success(f"{name}.{file_type} 下载成功")
+
+
+    此函数用于在爬虫任务中，
+    通过一个给定的文本元素xpath，快速地进行 文本 下载操作
+    """
+    text = extract_text_by_response_xpath(response, xpath)  # type: ignore
+    download_text(split.join(text), **kwargs)
+
+
+def download_image_by_response_xpath(
+        response,
+        xpath,
+        **kwargs
+):
+    """
+    :param response: 网页response
+    :param xpath: 网页中需要下载的图片元素xpath
+    :return: None
+
+    此函数用于在爬虫任务中，
+    通过一个给定的图片元素xpath，快速地进行 图片 下载操作
+    """
+    url_list = extract_link_by_response_xpath(response, xpath)
+    for url in url_list:
+        download_img(url, **kwargs)
+
+
+def download_file_by_response_xpath(
+        response,
+        xpath,
+        file_type,
+        **kwargs
+):
+    """
+    :param response: 网页response
+    :param xpath: 网页中需要下载的附件元素xpath
+    :param file_type: 下载文件的后缀名，如果没有指定，则默认为txt
+    :return: None
+
+    此函数用于在爬虫任务中，
+    通过一个给定的附件元素xpath，快速地进行 附件 下载操作
+    """
+    url_list = extract_link_by_response_xpath(response, xpath)
+    for url in url_list:
+        download_file(url, file_type, **kwargs)
